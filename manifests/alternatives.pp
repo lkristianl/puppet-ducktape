80445cdf5eea70bd76aaf288f8b86b43fbbd09e1
kris.sbitio <kris@sbit.io>
2021-09-02 16:23:15 +0200

    alternatives-module: Modernize ducktape

--- manifests/alternatives.pp
+++ manifests/alternatives.pp
@@ -1,11 +1,10 @@
 class ducktape::alternatives (
   Boolean $enabled = true,
+  Hash $defaults = {},
 ) {
 
   if $enabled {
-
-    $alternatives_defaults = hiera('ducktape::alternatives::defaults', {})
-    create_resources('alternatives', hiera_hash('ducktape::alternatives', {}), $alternatives_defaults)
+    require ::ducktape::alternatives::autoload
   }
 
 }
